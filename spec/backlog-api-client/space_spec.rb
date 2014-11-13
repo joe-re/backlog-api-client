addcb07795a5896137832caa345f6a1526176912
joe-re <joe.tialtngo@gmail.com>
2014-11-14 03:37:45 +0900

    add test of space/information

--- spec/backlog-api-client/space_spec.rb
+++ spec/backlog-api-client/space_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 describe BacklogApiClient::Client::Space do
-  it {}
+  describe '#information' do
+    before do
+      @client = double('client')
+      expect(@client).to receive(:get).with('space').and_return('ok')
+    end
+    subject { described_class.new(@client).information }
+    it { should eq 'ok' }
+  end
 end
 
