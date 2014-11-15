14e213987003e4a111f3e16163b83696621fca0b
joe-re <joe.tialtngo@gmail.com>
2014-11-15 19:43:06 +0900

    [refactor] standardize double object for spec

--- spec/backlog-api-client/issues_spec.rb
+++ spec/backlog-api-client/issues_spec.rb
@@ -1,22 +1,19 @@
 require 'spec_helper'
 
 describe BacklogApiClient::Client::Issues do
+  let(:client) { double('client') }
+
   describe '#get_information' do
     let(:test_issue_id) { 'TEST-ISSUE-ID' }
-    before do
-      @client = double('client')
-      expect(@client).to receive(:get).with("issues/#{test_issue_id}").and_return('ok')
-    end
-    subject { described_class.new(@client).get_information(test_issue_id) }
+    before { expect(client).to receive(:get).with("issues/#{test_issue_id}").and_return('ok') }
+    subject { described_class.new(client).get_information(test_issue_id) }
     it { should eq 'ok' }
   end
 
   describe '#create' do
-    before do
-      @client = double('client')
-      expect(@client).to receive(:post).with('issues', 'request').and_return('ok')
-    end
-    subject { described_class.new(@client).create('request') }
+    let(:request) { 'request' }
+    before { expect(client).to receive(:post).with('issues', request).and_return('ok') }
+    subject { described_class.new(client).create(request) }
     it { should eq 'ok' }
   end
 end
